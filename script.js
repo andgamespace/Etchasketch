e792b8ce055300b46be32a31fd36b00cfab2b300
andgamespace <anshxc@gmail.com>
2022-08-18 15:45:05 -0500

    Added color change function, still needs functionality

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 // cell.innerHTML = "Text";
 // container.appendChild(cell);
 const grid_size = 16;
+const colorChoice = "#0000FF";
 function loadGrid(sizeOfGrid) {
     grid.style.gridTemplateColumns = 'repeat(${size}, 1fr)'
     grid.style.gridTemplateRows = 'repeat(${size}, 1fr)'
@@ -10,7 +11,18 @@ function loadGrid(sizeOfGrid) {
     while (numGridElements < (sizeOfGrid*sizeOfGrid)){
         const gridElement = document.createElement('div')
         gridElement.classList.add("grid-element")
+        gridElement.addEventListener(hover, colorChange())
         grid.appendChild.gridElement;
         numGridElements++;
+    }
+}
+window.onload = () => {
+    loadGrid(grid_size)
+}
+function colorChange(a)
+{
+    if(a.type == 'mouseover'){
+        a.target.style.background = colorChoice;
+
     }
 }
