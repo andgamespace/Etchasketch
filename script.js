c9d8e50697569f56e115eb7e93fddcc28bd0aeb4
andgamespace <anshxc@gmail.com>
2022-09-03 21:20:25 -0500

    event listeners now work properly

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-let sizeOfGrid = 16;
-const container = document.querySelector(".grid-container");
-
+let sizeOfGrid = 256
+const grid = document.getElementById("grid")
+const GRAY = "#333333"
 function makeGrid() {
-
-    
+    grid.style.gridTemplateColumns = 'repeat(${size), 1fr)'
+    grid.style.gridTemplateRows = 'repeat(${size}, 1fr)'
+    for(let i=0; i<sizeOfGrid; i++){
+        const square = document.createElement('div')
+        square.classList.add("square")
+        square.addEventListener('mouseover', colorChange)
+        square.addEventListener('mousedown', colorChange)
+        grid.appendChild(square)
+    }
 
 }
-for(let i=0; i<sizeOfGrid**2; i++){
-    const square = document.createElement('div');
-    square.classList.add("grid-item");
-    square.setAttribute("draggable","false");
-    square.style.backgroundColor = "transparent";
-    container.appendChild(square);
+function colorChange(input) {
+    if(input.type == 'mouseover' && !'mousedown') return
+    input.target.style.backgroundColor = GRAY;
+}
+window.onload = () => {
+    makeGrid()
 }
